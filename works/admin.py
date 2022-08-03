8c6093527712ab5c6a4ed0f19c9466ba0247c24d
Dilshod <murodkobilovdilshod@mail.ru>
2022-08-03 18:54:40 +0500

    added works model

--- works/admin.py
+++ works/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from  .models import WorksCategoryModel, WorksModel
+
+@admin.register(WorksCategoryModel)
+
+class WorksCategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+
+@admin.register(WorksModel)
+class WorksModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at']
+    list_display_links = ['id', 'title', 'created_at']
+    search_fields = ['title']
+
+
